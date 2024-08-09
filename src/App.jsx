10a7ee31d4c3f70b1fbde4867930414b1898b9e4
GG-Pharaoh <oxdiablo20@gmail.com>
2024-08-09 09:28:22 -0700

    first deployment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import RandomQuote from './Components/RandomQuote/RandomQuote'
+
+
+function App() {
+ 
+
+  return (
+  <div> 
+    <RandomQuote/>
+
+  </div>
+  )
+}
+
+export default App
